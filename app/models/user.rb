d0b3b505d25cfec0f602c57e3f275f6fd3694fcd
Roberto-Luis-Rodriguez <robertorodriguezjr86@gmail.com>
2018-01-23 14:32:59 +0000

    Finish user edit, update, index, and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ validates :email, presence: true, length: { maximum: 255 },
                   format: { with: VALID_EMAIL_REGEX },
                   uniqueness: { case_sensitive: false }
 has_secure_password
-validates :password, presence: true, length: { minimum: 6 }
+validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 mount_uploader :profile_photo, ProfilePhotoUploader
 
 # Returns the hash digest of the given string.
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180118192954) do
+ActiveRecord::Schema.define(version: 20180123134826) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20180118192954) do
     t.string "password_digest"
     t.string "profile_photo"
     t.string "remember_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
